8b7515542b175e75248c51cfac95d6afa45ed3e9
lineage2m-jg8 <60877594+lineage2m-jg8@users.noreply.github.com>
2020-04-10 18:00:55 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1881,7 +1881,7 @@ while True:
 									hours, remainder = divmod(total_seconds,60*60)
 									minutes, seconds = divmod(remainder,60)
 
-									result_lefttime += '다음 ' + ouput_bossData[i][0] + '탐까지 %02d:%02d:%02d 남았습니다. ' % (hours,minutes,seconds) + '[' +  ouput_bossData[i][2] + ']\n'
+									result_lefttime += '[' +  ouput_bossData[i][2]+ '] ' + ouput_bossData[i][0] + ' 탐까지 %02d시 %02d분 %02d초 남았습니다. ' % (hours, minutes, seconds) + '\n'
 					else :
 						for j in range(len(sorted_datelist)):
 							for i in range(len(ouput_bossData)):						
@@ -1892,7 +1892,7 @@ while True:
 									hours, remainder = divmod(total_seconds,60*60)
 									minutes, seconds = divmod(remainder,60)
 
-									result_lefttime += '다음 ' + ouput_bossData[i][0] + '탐까지 %02d:%02d:%02d 남았습니다. ' % (hours,minutes,seconds) + '[' +  ouput_bossData[i][2] + ']\n'
+									result_lefttime += '[' +  ouput_bossData[i][2]+ '] ' + ouput_bossData[i][0] + ' 탐까지 %02d시 %02d분 %02d초 남았습니다. ' % (hours, minutes, seconds) + '\n'
 					embed = discord.Embed(
 						description= result_lefttime,
 						color=0xff0000
